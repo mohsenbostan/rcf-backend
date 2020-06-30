2b4a3554e6cc9fc35b508e9d0b04de9634f6737c
Mohsen Bostan <mohsenbostandev@gmail.com>
2020-06-30 19:14:12 +0430

    Fix Routes Issue

--- src/routes/api.php
+++ src/routes/api.php
@@ -18,17 +18,17 @@ Route::prefix('v1/')->group(function () {
 
     // Authentication Routes
     Route::prefix('/auth')->group(function () {
-        Route::post('/register', 'API\V01\Auth\AuthController@register')->name('auth.register');
-        Route::post('/login', 'API\V01\Auth\AuthController@login')->name('auth.login');
-        Route::get('/user', 'API\V01\Auth\AuthController@user')->name('auth.user');
-        Route::post('/logout', 'API\V01\Auth\AuthController@logout')->name('auth.logout');
+        Route::post('/register', 'API\v1\Auth\AuthController@register')->name('auth.register');
+        Route::post('/login', 'API\v1\Auth\AuthController@login')->name('auth.login');
+        Route::get('/user', 'API\v1\Auth\AuthController@user')->name('auth.user');
+        Route::post('/logout', 'API\v1\Auth\AuthController@logout')->name('auth.logout');
     });
 
     // Channel Route
     Route::prefix('/channel')->group(function () {
-        Route::get('/all', 'API\V01\Channel\ChannelController@getAllChannelsList')->name('channel.all');
-        Route::post('/create', 'API\V01\Channel\ChannelController@createNewChannel')->name('channel.create');
-        Route::put('/update', 'API\V01\Channel\ChannelController@updateChannel')->name('channel.update');
-        Route::delete('/delete', 'API\V01\Channel\ChannelController@deleteChannel')->name('channel.delete');
+        Route::get('/all', 'API\v1\Channel\ChannelController@getAllChannelsList')->name('channel.all');
+        Route::post('/create', 'API\v1\Channel\ChannelController@createNewChannel')->name('channel.create');
+        Route::put('/update', 'API\v1\Channel\ChannelController@updateChannel')->name('channel.update');
+        Route::delete('/delete', 'API\v1\Channel\ChannelController@deleteChannel')->name('channel.delete');
     });
 });
