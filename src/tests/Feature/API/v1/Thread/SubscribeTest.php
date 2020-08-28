cc6578be5201372a520df8374ee8f7ae84f688db
Mohsen Bostan <mohsenbostandev@gmail.com>
2020-08-28 11:56:18 +0430

    Add Test For NewReplySubmitted Notification

--- src/tests/Feature/API/v1/Thread/SubscribeTest.php
+++ src/tests/Feature/API/v1/Thread/SubscribeTest.php
@@ -2,11 +2,14 @@
 
 namespace Tests\Feature\API\v1\Thread;
 
+use App\Answer;
 use App\Channel;
+use App\Notifications\NewReplySubmitted;
 use App\Thread;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Notification;
 use Laravel\Sanctum\Sanctum;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
@@ -39,5 +42,32 @@ class SubscribeTest extends TestCase
         $response->assertJson([
             'message' => 'user unsubscribed successfully'
         ]);
+    }
+
+    /** @test */
+    public function notification_will_send_to_subscribers_of_a_thread()
+    {
+        $user = factory(User::class)->create();
+        Sanctum::actingAs($user);
+        Notification::fake();
+
+        $thread = factory(Thread::class)->create();
+
+        $subscribe_response = $this->post(route('subscribe', [$thread]));
+        $subscribe_response->assertSuccessful();
+        $subscribe_response->assertJson([
+            'message' => 'user subscribed successfully'
+        ]);
+
+        $answer_response = $this->postJson(route('answers.store'), [
+            'content' => 'Foo',
+            'thread_id' => $thread->id
+        ]);
+        $answer_response->assertSuccessful();
+        $answer_response->assertJson([
+            'message' => 'answer submitted successfully'
+        ]);
+
+        Notification::assertSentTo($user, NewReplySubmitted::class);
     }
 }
